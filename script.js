873ef6119f85bc8df711f06f89bb1026400dfcef
RohanScripts <shelarrohan1810@gmail.com>
2025-01-10 11:06:03 +0530

    Add history feature to calculator for tracking calculations

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const display = document.querySelector("#result");
 const buttons = document.querySelectorAll(".bb");
-
+const historyList = document.querySelector("#history-listing")
+let historyArray = []
 for(const eachButton of buttons){
     eachButton.addEventListener("click",function(){
-           
-
         if(eachButton.innerHTML==="AC"){
             display.innerText = ""
         }
         else if(eachButton.innerHTML==="="){
-            display.innerText = eval(display.innerText)
+            const result = eval(display.innerText)
+            historyArray.push(`${display.innerText} = ${result}`)
+            showHistory()
+            display.innerText = result
         }
         else{
             display.innerText = display.innerText + eachButton.innerHTML 
         }
     })
-    
 }
 
+const showHistory = () => {
+    historyList.innerHTML = ""
+    historyArray.map((eachHistory)=>{
+        let listItem = document.createElement("li");
+        listItem.innerText = eachHistory
+        historyList.appendChild(listItem)
+    })
+}
